31f9f0e6efdde57455b08276fa9939e08446992c
bcgodev <21715194+bcgodev@users.noreply.github.com>
2021-07-25 14:01:04 +0800

    feat: upload buffer directly without file io

--- utils/image-utils/uploadToGCSHandler.js
+++ utils/image-utils/uploadToGCSHandler.js
@@ -1,47 +1,21 @@
-const fs = require('fs')
 const imageUrlBase = 'assets/images/'
-const { deleteImageFromLocal } = require('../image-utils/localImageFileHandler')
 
-function uploadImagesToGCS(imageNameList, bucket) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            // need to ensure first image is uploaded
-            // (or CMS won't have file snapshot)
-            await saveLocalImageToGCS(imageNameList[0], bucket)
-
-            // however, for loop can't handle async function
-            // so we need to seperate first upload
-            for (let i = 1; i < imageNameList.length; i++) {
-                saveLocalImageToGCS(imageNameList[i], bucket)
-            }
-
-            resolve()
-        } catch (err) {
-            reject(`error in upload images to GCS, ${err}`)
-        }
-    })
-}
-function saveLocalImageToGCS(fileName, bucket) {
-    const stream = fs.createReadStream(`./public/images/${fileName}`)
-
-    return new Promise(async (resolve, reject) => {
-        const gcsUploadPath = `${imageUrlBase}${fileName}`
-        const file = bucket.file(gcsUploadPath) //get the upload path
-        const write = file
-            .createWriteStream()
-            .on('finish', (data) => {
-                console.log(`${fileName} has been uploaded to GCS`)
-
-                // after update to gcs, delete local image
-                deleteImageFromLocal(fileName)
-                resolve()
-            })
-            .on('error', (err) => {
-                reject(`error in upload image to GCS, ${err}`)
-            })
-
-        stream.pipe(write)
+function uploadBufferToGCS(fileName, buffer, bucket) {
+    const gcsUploadPath = `${imageUrlBase}${fileName}`
+    return new Promise((resolve, reject) => {
+        let stream = bucket.file(gcsUploadPath).createWriteStream({
+            contentType: 'auto',
+            gzip: true,
+            resumable: false,
+        })
+        stream.end(buffer)
+        stream.once('error', (err) => {
+            reject(err)
+        });
+        stream.once('finish', () => {
+            resolve(gcsUploadPath)
+        });
     })
 }
 
-module.exports = { uploadImagesToGCS }
+module.exports = { uploadBufferToGCS }
