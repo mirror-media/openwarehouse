33c4b2d63179e3eab8c6f78ff996dc61139317bd
bcgodev <21715194+bcgodev@users.noreply.github.com>
2021-11-03 19:29:05 +0800

    feat(lists/readr): add migration script
    Refer: https://app.asana.com/0/1201268625767017/1201277262827282/f

--- lists/readr/db/20211101_UPDATE_POST_TA_CATEGORY.sql
+++ lists/readr/db/20211101_UPDATE_POST_TA_CATEGORY.sql
@@ -1,39 +1,47 @@
+
+BEGIN;
+
 -- public."Post" new definition
 
-ALTER TABLE public."Post" ADD "readingTime" int4 NULL;
-ALTER TABLE public."Post" ADD "actionList" text NULL;
-ALTER TABLE public."Post" ADD citation text NULL;
-ALTER TABLE public."Post" ADD "citationApiData" text NULL;
-ALTER TABLE public."Post" ADD "citationHtml" text NULL;
+ALTER TABLE "Post" ADD "readingTime" int4 NULL;
+ALTER TABLE "Post" ADD "actionList" text NULL;
+ALTER TABLE "Post" ADD citation text NULL;
+ALTER TABLE "Post" ADD "citationApiData" text NULL;
+ALTER TABLE "Post" ADD "citationHtml" text NULL;
+
+-- Rename Post_tags_many to Post_tags_Tag_relatedPost
+
+ALTER TABLE "Post_tags_many" RENAME TO "Post_tags_Tag_relatedPost";
+
+ALTER TABLE "Post_tags_Tag_relatedPost" RENAME CONSTRAINT post_tags_many_post_left_id_foreign TO post_tags_tag_relatedpost_post_left_id_foreign;
+ALTER TABLE "Post_tags_Tag_relatedPost" RENAME CONSTRAINT post_tags_many_tag_right_id_foreign TO post_tags_tag_relatedpost_tag_right_id_foreign;
+
+DROP INDEX post_tags_many_post_left_id_index;
+DROP INDEX post_tags_many_tag_right_id_index;
+CREATE INDEX post_tags_tag_relatedpost_post_left_id_index ON "Post_tags_Tag_relatedPost" USING btree ("Post_left_id");
+CREATE INDEX post_tags_tag_relatedpost_tag_right_id_index ON "Post_tags_Tag_relatedPost" USING btree ("Tag_right_id");
 
 
--- public."Category_relatedPost_many" definition
+-- Rename Post_categories_many to Category_relatedPost_Post_categories
 
-CREATE TABLE "Category_relatedPost_many" (
-	"Category_left_id" int4 NOT NULL,
-	"Post_right_id" int4 NOT NULL
-);
-CREATE INDEX category_relatedpost_many_category_left_id_index ON public."Category_relatedPost_many" USING btree ("Category_left_id");
-CREATE INDEX category_relatedpost_many_post_right_id_index ON public."Category_relatedPost_many" USING btree ("Post_right_id");
+ALTER TABLE "Post_categories_many" RENAME TO "Category_relatedPost_Post_categories";
 
+DROP INDEX post_categories_many_category_right_id_index;
+DROP INDEX post_categories_many_post_left_id_index;
 
--- public."Category_relatedPost_many" foreign keys
+ALTER TABLE "Category_relatedPost_Post_categories" DROP CONSTRAINT post_categories_many_category_right_id_foreign;
+ALTER TABLE "Category_relatedPost_Post_categories" DROP CONSTRAINT post_categories_many_post_left_id_foreign;
 
-ALTER TABLE public."Category_relatedPost_many" ADD CONSTRAINT category_relatedpost_many_category_left_id_foreign FOREIGN KEY ("Category_left_id") REFERENCES "Category"(id) ON DELETE CASCADE;
-ALTER TABLE public."Category_relatedPost_many" ADD CONSTRAINT category_relatedpost_many_post_right_id_foreign FOREIGN KEY ("Post_right_id") REFERENCES "Post"(id) ON DELETE CASCADE;
+ALTER TABLE "Category_relatedPost_Post_categories" RENAME COLUMN "Post_left_id" TO "Post_right_id";
+ALTER TABLE "Category_relatedPost_Post_categories" RENAME COLUMN "Category_right_id" TO "Category_left_id";
 
+ALTER TABLE "Category_relatedPost_Post_categories"
+    ADD CONSTRAINT category_relatedpost_post_categories_category_left_id_foreign FOREIGN KEY ("Category_left_id") REFERENCES "Category"(id) ON DELETE CASCADE;
 
--- public."Tag_relatedPost_many" definition
+ALTER TABLE "Category_relatedPost_Post_categories"
+    ADD CONSTRAINT category_relatedpost_post_categories_post_right_id_foreign FOREIGN KEY ("Post_right_id") REFERENCES "Post"(id) ON DELETE CASCADE;
 
-CREATE TABLE "Tag_relatedPost_many" (
-	"Tag_left_id" int4 NOT NULL,
-	"Post_right_id" int4 NOT NULL
-);
-CREATE INDEX tag_relatedpost_many_post_right_id_index ON public."Tag_relatedPost_many" USING btree ("Post_right_id");
-CREATE INDEX tag_relatedpost_many_tag_left_id_index ON public."Tag_relatedPost_many" USING btree ("Tag_left_id");
+CREATE INDEX category_relatedpost_post_categories_category_left_id_index ON "Category_relatedPost_Post_categories" USING btree ("Category_left_id");
+CREATE INDEX category_relatedpost_post_categories_post_right_id_index ON "Category_relatedPost_Post_categories" USING btree ("Post_right_id");
 
-
--- public."Tag_relatedPost_many" foreign keys
-
-ALTER TABLE public."Tag_relatedPost_many" ADD CONSTRAINT tag_relatedpost_many_post_right_id_foreign FOREIGN KEY ("Post_right_id") REFERENCES "Post"(id) ON DELETE CASCADE;
-ALTER TABLE public."Tag_relatedPost_many" ADD CONSTRAINT tag_relatedpost_many_tag_left_id_foreign FOREIGN KEY ("Tag_left_id") REFERENCES "Tag"(id) ON DELETE CASCADE;
+COMMIT;
