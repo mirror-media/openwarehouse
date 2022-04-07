d9c9ff6e6fd879f344c9cdc06c0389d4fc0bd05b
tingwei <yatiti84@gmail.com>
2022-04-07 16:36:07 +0800

    db migrtion alter sale and column logo in table spnsor

---
+++ lists/mirror-tv/pq/migrations/014_ALTER_TABLE_SALE_AND_SPONSER_LOGO.sql
@@ -0,0 +1,16 @@
+ALTER TABLE "Sale" ADD COLUMN "choice" int4 NOT NULL;
+ALTER TABLE "Sale" ADD COLUMN "state" text NOT NULL;
+
+
+ALTER TABLE "Sale" DROP COLUMN "status";
+ALTER TABLE "Sale" DROP COLUMN "adPost";
+
+
+ALTER TABLE "Sale" DROP COLUMN "choice";
+ALTER TABLE "Sale" ADD COLUMN "adPost" int4 NOT NULL;
+ALTER TABLE "Sponsor" ALTER COLUMN "logo" SET NOT NULL;
+
+ALTER TABLE "Sale" ADD COLUMN "status" text NOT NULL;
+ALTER TABLE "Sale" DROP COLUMN "state";
+ALTER TABLE "Sale" ADD CONSTRAINT "Sale_status_check" CHECK ((status = ANY (ARRAY['draft'::text, 'published'::text, 'scheduled'::text, 'archived'::text])));
+CREATE INDEX sale_adPost_index ON "Sale" USING btree (adPost);
